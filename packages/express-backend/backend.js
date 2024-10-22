de0cf0fd69ed866177d43031f46876a7769a17f9
Charlie Jansen <charliejansen2@gmail.com>
2024-10-21 22:35:58 -0700

    Added mongoose/MongoDB functionality

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,5 +1,17 @@
 import express, { json } from "express";
+import { addUser, getUsers, findUserById, findByIdAndDelete } from "./services/user-service.js";
 import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const { MONGO_CONNECTION_STRING } = process.env;
+
+mongoose.set("debug", true);
+mongoose
+  .connect(MONGO_CONNECTION_STRING)
+  .catch((error) => console.log(error));
 
 const app = express();
 const port = 8000;
@@ -14,21 +26,13 @@ app.get("/", (req, res) => {
 app.get("/users", (req, res) => {
   const name = req.query.name;
   const job = req.query.job;
-  if (name != undefined) {
-    if (job != undefined) {
-      let result = findUserByNameAndJob(name, job);
-      result = { users_list: result };
-      res.send(result);
-    }
-    else {
-      let result = findUserByName(name);
-      result = { users_list: result };
-      res.send(result);
-    }
+ getUsers(name, job).then((result) => {
+  if (result) { 
+    res.send(result);
   } else {
-    res.send(users);
-  }
-});
+    res.status(404).send('Not Found: ${id}');
+ }
+})});
 
 app.listen(port, () => {
   console.log(
@@ -36,7 +40,7 @@ app.listen(port, () => {
   );
 });
 
-const findUserByName = (name) => {
+/*const findUserByName = (name) => {
   return users["users_list"].filter(
     (user) => user["name"] === name
   );
@@ -49,7 +53,7 @@ const findUserByNameAndJob = (name, job) => {
 };
 
 const findUserById = (id) =>
-  users["users_list"].find((user) => user["id"] === id);
+  users["users_list"].find((user) => user["id"] === id);*/
 
 app.get("/users/:id", (req, res) => {
   const id = req.params["id"]; //or req.params.id
@@ -61,11 +65,11 @@ app.get("/users/:id", (req, res) => {
   }
 });
 
-const addUser = (user) => {
+/*const addUser = (user) => {
   user["id"] = Math.floor(Math.random() * 99999)
   users["users_list"].push(user);
   return user;
-};
+};*/
 
 app.post("/users", (req, res) => {
   const userToAdd = req.body;
@@ -79,40 +83,9 @@ const removeUser = (userToRemove) => {
 };
 
 app.delete("/users/:id", (req, res) => {
-  const id = req.params["id"]; //or req.params.id
-  const userToRemove = users["users_list"].find((user) => user["id"] === id);
-  removeUser(userToRemove);
-  res.status(204).send()
-  //res.status(205).send("abcd")
-});
-
-
-const users = {
-    users_list: [
-      {
-        id: "xyz789",
-        name: "Charlie",
-        job: "Janitor"
-      },
-      {
-        id: "abc123",
-        name: "Mac",
-        job: "Bouncer"
-      },
-      {
-        id: "ppp222",
-        name: "Mac",
-        job: "Professor"
-      },
-      {
-        id: "yat999",
-        name: "Dee",
-        job: "Aspring actress"
-      },
-      {
-        id: "zap555",
-        name: "Dennis",
-        job: "Bartender"
-      }
-    ]
-  };
+  /*const id = req.params["id"]; //or req.params.id
+  const userToRemove = users["users_list"].find((user) => user["id"] === id);*/
+  findByIdAndDelete(req.params["id"]).then((result) => {
+  if (result) {res.status(204).send()
+  }
+});});
--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -7,7 +7,7 @@ function MyApp() {
   const [characters, setCharacters] = useState([]);
 
   function deleteUser(person) {
-    const promise = fetch("http://localhost:8000/users/" + person["id"], {
+    const promise = fetch("http://localhost:8000/users/" + person["_id"], {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json"
@@ -39,7 +39,7 @@ function MyApp() {
   useEffect(() => {
     fetchUsers()
       .then((res) => res.json())
-      .then((json) => setCharacters(json["users_list"]))
+      .then((json) => setCharacters(json))
       .catch((error) => {
         console.log(error);
       });
